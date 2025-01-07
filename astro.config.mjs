4b19a1b50e861204959fd09f361e0991ec5ebb10
Aditya <meetadityaagarwal@gmail.com>
2025-01-07 12:14:50 +0530

    fix sanity dashboard

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
-import dotenv from 'dotenv';
-
-dotenv.config();
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+
 import vercel from '@astrojs/vercel';
+
 import sanity from '@sanity/astro';
 
 export default defineConfig({
   integrations: [tailwind(), react(), sanity({
-    projectId: process.env.SANITYIO_ID,
-    dataset: process.env.SANITYIO_DATASET,
+    projectId: 'y970ornb',
+    dataset: 'production',
     // Set useCdn to false if you're building statically.
     useCdn: false,
     studioBasePath: '/admin'
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,16 +2,13 @@ import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemaTypes'
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default defineConfig({
   name: 'default',
   title: 'Brahmgange',
 
-  projectId: process.env.SANITYIO_ID,
-  dataset: process.env.SANITYIO_DATASET,
+  projectId: 'y970ornb',
+  dataset: 'production',
 
   plugins: [structureTool(), visionTool()],
 
